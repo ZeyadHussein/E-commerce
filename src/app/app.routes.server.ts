d03c664851fcc01e3ff5bd8e6c3696ec0023379d
Zeyad Hussein <zeyad02_hussein@hotmail.com>
2026-02-14 20:27:58 +0200

    first commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product-details/:id',
+    renderMode: RenderMode.Server  // No build-time API call needed
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
